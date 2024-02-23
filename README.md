60ac6fc3892731d5fcf979bb87b1dfefb95bab54
Lisa Donohoo <lisa.donohoo@gmail.com>
2024-02-22 21:20:48 -0600

    Test

---
+++ README.md
@@ -0,0 +1,2 @@
+# resume-project-V3
+New version of resume project with media queries, accessibility, and future features to implement on cloud
---
+++ scripts/resume-v3.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+console.log('helloww');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+addEventListener("resize", (event) => {
+    let documentWidth = document.documentElement.clientWidth;
+    console.log(documentWidth);
+    let currWidth = document.createTextNode(documentWidth);
+    console.log(currWidth);
+    document.getElementById('title-bar').appendChild.currWidth;
+        // var theDiv = document.getElementById("<ID_OF_THE_DIV>");
+        //  var content = document.createTextNode("<YOUR_CONTENT>");
+       // theDiv.appendChild(content);
+}); */
